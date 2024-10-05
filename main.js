cb1780b0d2354e4d2f1c187b94bb5a4ac4ccd1d4
Calvin Crady <calvin.crady@gmail.com>
2024-10-05 16:12:32 -0700

    Add in all the planets

--- main.js
+++ main.js
@@ -7,30 +7,32 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry( 1, 32, 16 );
+const geometry = new THREE.SphereGeometry( 0.5, 32, 16 );
 const material = new THREE.MeshNormalMaterial();
 material.flatShading = true;
-const planet = new THREE.Mesh( geometry, material );
-scene.add( planet );
-camera.position.z = 5;
+
+let planets = {}
+for (let property in keplerParams) {
+    let planet = new THREE.Mesh( geometry, material );
+    planets[property] = planet;
+    scene.add(planet);
+}
+camera.position.z = 10;
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 const clock = new THREE.Clock();
 
 
 function animate() {
-    let predictedCoords = findCoords(
-        keplerParams.mars,
-        clock.getElapsedTime() / 300,
-        0.0000001,
-    );
-    //predictedCoords.divideScalar(100);
-
-    planet.position.set(predictedCoords.x, predictedCoords.y, predictedCoords.z);
-    //camera.lookAt(planet.position);
-
+    for (let property in keplerParams) {
+        let predictedCoords = findCoords(
+            keplerParams[property],
+            clock.getElapsedTime() / 300,
+            0.0000001,
+        );
+        planets[property].position.set(predictedCoords.x, predictedCoords.y, predictedCoords.z);
+    }
     renderer.render( scene, camera );
-    console.log(predictedCoords);
 }
 renderer.setAnimationLoop( animate );
 
